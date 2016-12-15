432c384737f006475f0adf4f3a6fb7ebdec702dd
alex <alex@alexs-MacBook-Pro-2.local>
2016-12-15 16:01:46 +1100

    email working locally, without queue or send later, about to test deployed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,23 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:                'smtp.gmail.com',
+      port:                   587,
+      # domain:               'localhost:3000',
+      user_name:              ENV["GMAIL_USERNAME"],
+      password:               ENV["GMAIL_PASSWORD"],
+      authentication:         'plain',
+      enable_starttls_auto:   true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    }
+
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_options = {from: 'alxhdevelopment@gmail.com'}
+  #mailer setting ends
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
