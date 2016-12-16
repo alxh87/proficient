427b01d1df6e6e3d913269ba5bfbc217dd8eaab4
alex <alex@alexs-MacBook-Pro-2.local>
2016-12-17 00:28:45 +1100

    added validation error messages to numbers, added mon-fri check to officehours, moved officehour check to first voice response, added all current numbers to verified senders

--- app/models/support_number.rb
+++ app/models/support_number.rb
@@ -1,3 +1,3 @@
 class SupportNumber < ActiveRecord::Base
-	validates :number, format: { with: /\A\+\d{11}\z/}
+	validates :number, format: { with: /\A\+\d{11}\z/, message: 'Please enter number in format +612345678901'}
 end
